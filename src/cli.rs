ad118976f6280f4b345472c30046a6653f215cad
Wizdan M. G. S. <wizdanshodri@protonmail.com>
2026-01-12 18:53:42 +0700

    feat: add command description

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,13 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(name = "todo")]
-#[command(about = "Simple CLI todo app")]
+#[command(
+    name = "todo",
+    version,
+    about = "A simple CLI todo list application",
+    long_about = None,
+    after_help = "Data is stored locally in a JSON file."
+)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
@@ -10,8 +15,27 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
-    Add { title: String },
+    /// Add a new todo item
+    ///
+    /// Example:
+    ///   todo add "Buy a bottle of milk"
+    Add {
+        /// The title or description of the todo
+        title: String,
+    },
+
+    /// List all todo items
     List,
-    Done { id: u32 },
-    Delete { id: u32 },
+
+    /// Mark a todo item as completed
+    Done {
+        /// ID of the todo to mark as done
+        id: u32,
+    },
+
+    /// Delete a todo item
+    Delete {
+        /// ID of the todo to delete
+        id: u32,
+    },
 }
